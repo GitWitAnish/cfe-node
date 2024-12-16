3aed0688b21a4e6ecbc053c5bd1e8f9c4a46697a
Anish Karki <anishkarki5710@gmail.com>
2024-12-16 22:30:52 +0545

    Failed subject

--- Assignment/Day4.js/4.js
+++ Assignment/Day4.js/4.js
@@ -4,4 +4,24 @@ const students = [
     { name: "Hari", age: 20, scores: [95, 85, 90] },
     { name: "Gopal", age: 21, scores: [55, 60, 65] },
   ];
+
+
+
+
+
+  function failedSubjects(students) {
+    const failures = {};
   
+    students.forEach(student => {
+      student.scores.forEach((score, index) => {
+        if (score < 60) {
+          failures[index] = (failures[index] || 0) + 1;
+        }
+      });
+    });
+  
+    return failures;
+  }
+
+
+  console.log("Failed Subjects:", failedSubjects(students));
