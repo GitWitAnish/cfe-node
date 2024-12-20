c5f04757d589a183b2960083180bd863b03e4eab
Anish Karki <anishkarki5710@gmail.com>
2024-12-20 08:48:45 +0545

    Guess the number

--- projects/Guess the number/guess_the_number.js
+++ projects/Guess the number/guess_the_number.js
@@ -1,27 +1,42 @@
 const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  
-  const number = Math.floor(Math.random() * 100) + 1;
-  
-  function ask() {
-    readline.question('Guess a number between 1 and 100: ', guess => {
-      const num = parseInt(guess);
-      if (num === number) {
-        console.log('Congratulations! You guessed the number.');
-        readline.close();
-      } else {
-        const diff = num - number;
-        if (diff > 10) {
-          console.log(num < number ? 'Too low! Try again.' : 'Too high! Try again.');
-        } else {
-          console.log(num < number ? 'A little higher! Try again.' : 'A little lower! Try again.');
+  input: process.stdin,
+  output: process.stdout
+});
+
+const number = Math.floor(Math.random() * 100) + 1;
+
+function askNum() {
+  readline.question('Guess a number between 1 and 100: ', guess => {
+    const num = parseInt(guess);
+
+    if (num === number) {
+      console.log('Congratulations! You guessed the number.');
+      readline.close();
+    }
+    
+    else {
+      const diff = Math.abs(num - number);
+
+      if (diff > 10) {
+        if (num < number) {
+          console.log('Too low! Try again.');
+        } else if (num > number) {
+          console.log('Too high! Try again.');
         }
-        ask();
+      } 
+      
+      else {
+        if (num < number) {
+          console.log('A little higher! Try again.');
+        } else if (num > number) {
+          console.log('A little lower! Try again.');
+        }
       }
-    });
-  }
-  
-  ask();
-  
+
+      askNum();
+
+    }
+  });
+}
+
+askNum();
