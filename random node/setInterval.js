1b5db9765d9f2a7af85a2804b0b9541bb406f5bf
GitWitAnish <anishkarki5710@gmail.com>
2025-05-19 08:50:48 +0545

    interval and timeout

---
+++ random node/setInterval.js
@@ -0,0 +1,6 @@
+// setInterval() function lets you run a code repeatedly after a fixed interval.
+
+
+setInterval(function() {
+    console.log('This message repeats every 3 seconds.');
+  }, 3000); // 3000 milliseconds = 3 seconds
---
+++ random node/setTimeout.js
@@ -0,0 +1,8 @@
+// Synatax
+// setTimeout(function, milliseconds);
+
+
+
+console.log("Task: Sweep the floor");
+setTimeout(() => console.log("Task: Water the plants (delayed by 2 seconds)."), 2000);
+console.log("Task: Feed the cat");
